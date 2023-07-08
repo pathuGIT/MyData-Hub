49ad170d7750f2ee76a3769a437a9e4102de6e52
pathuGIT <pathum_20211057@fot.ruh.ac.lk>
2023-07-08 23:00:48 +0530

    update

---
+++ inc/connect.php
@@ -0,0 +1,11 @@
+<?php
+    $conn = mysqli_connect("localhost","root","","datahub");
+
+    if(!$conn){
+        die("does not connect".mysqli_error($conn));
+    }else{
+        echo"Connected successfully";
+    }
+
+    mysqli_close($conn);
+?>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-
+    echo"<h2>Hello</h2>";
 ?>
