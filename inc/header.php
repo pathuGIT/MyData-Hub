10be0273a888dc311a96e8a13d8522a268267677
pathuGIT <pathum_20211057@fot.ruh.ac.lk>
2023-07-10 16:31:40 +0530

    update 4

---
+++ inc/header.php
@@ -0,0 +1,40 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/copy.css">
+    <link rel="stylesheet" href="../css/home.css">
+    <script src="js/code.jquery.com_jquery-3.7.0.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <div class="row1">
+            <div class="cont">
+                <li class="logo">
+                    <img src="../img/logo.png" alt="logo">
+                </li>
+                <li>MyData <span>hub</span></li>
+            </div>
+            
+
+        </div>
+        <div class="row2">
+            <li> <a href="index.php">HOME</a></li>
+            <li> <a href="addData.php"> ADD DATA</a></li>
+            <li> <a href="showData.php"> SHOW DATA</a></li>
+            <li> <a href="#"></a> MAIL</li>
+            <li id="form" onclick="fun()">LOGOUT</li>
+        </div>
+
+        
+        <script>
+            function fun(){
+                window.location="../index.php";
+            }
+
+        </script>
